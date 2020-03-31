63da74055dc9ab7b5bc03128a6da7f6425882ecd
j.takubo <j.takubo@datumstudio.jp>
2020-04-01 02:16:38 +0900

    adversarially RL was achieved but some points are not clear for me.

--- utils.py
+++ utils.py
@@ -1,3 +1,17 @@
+
+import numpy as np
+import gym
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from tqdm.notebook import tqdm
+from kaggle_environments import evaluate, make
+import kaggle_environments
+
+import pdb; pdb.set_trace()
+
+
 class ConnectX(gym.Env):
     def __init__(self, switch_prob=0.5):
         self.env = make('connectx', debug=False)
